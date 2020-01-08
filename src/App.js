e06536049baacde19487b04058fecadf397972e4
bella-cockrell <heybellac@gmail.com>
2020-01-08 17:09:37 +0000

    Change the welcome message to <h1>

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Welcome to Kodflix
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
