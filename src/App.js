1d7a0900981c7c9193acbdf30ff688f326fbf75b
bella-cockrell <heybellac@gmail.com>
2020-01-13 19:55:52 +0000

    Adds additional styling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function Movie (props) {
   return (
     <div>
       <h3>{props.title}</h3>
-      <h3>{props.rating}</h3>
+      <h3>Rating: {props.rating}</h3>
     </div>
   )
 }
@@ -23,20 +23,36 @@ function Movie (props) {
 function App() {
   return (
     <div className='App'>
+      <body>
+
       <div className='container'>
-        <Movie title='Start Wars: The Rise of the Skywalker' rating='4'/>
+        <div className='item'>
+        <Movie title='Star Wars: The Rise of the Skywalker' rating='4'/>
         <img src= {starWars} alt= "Star Wars: The Rise of the Skywalker" />
+        </div>
+        <div className='item'>
         <Movie title='Jojo Rabbit' rating='9'/>
         <img src= {jojoRabbit} alt= "Jojo Rabbit" />
+        </div>
+        <div className='item'>
         <Movie title='The Witcher' rating='6'/>
         <img src= {theWitcher} alt= "The Witcher" />
+        </div>
+        <div className='item'>
         <Movie title='Dracula' rating='3'/>
         <img src= {dracula} alt= "Dracula" />
+        </div>
+        <div className='item'>
         <Movie title='Goodbye Lenin' rating='7'/>
         <img src= {goodbyeLenin} alt= "Goodbye Lenin" />
+        </div>
+        <div className='item'>
         <Movie title='Little Miss Sunshine' rating='7'/>
         <img src= {littleMiss} alt= "Little Miss Sunshine" />
+        </div>
       </div>
+
+      </body>
     </div>
   );
 }
