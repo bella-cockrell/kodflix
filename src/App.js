3c2dbef9c4d4c5a378a2276365e4c0e92155813d
bella-cockrell <heybellac@gmail.com>
2020-01-20 20:18:45 +0000

    Adding Route components and functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 //Routing
-import './Details.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from 'react-router-dom';
 //Style
 import './App.css';
 //Component
@@ -9,12 +15,35 @@ import Gallery from './Gallery';
 //App
 function App() {
   return (
-    <div className='App'>
-      <meta content='width=device-width, initial-scale=1' name='viewport' />
-        <h1> Kodflix </h1>
-        <Gallery />
-    </div>
+    <Router>
+        <div className='App'>
+          <h1>Kodflix</h1>
+          <Route path='/' exact component={Gallery} />
+          <Route path='/films' exact component={Films} />
+          <Route path='/contactus' exact component={ContactUs} />
+        </div>
+    </Router>
   );
 }
 
+function Films() {
+  return (
+  <div>
+    <div>Welcome to Films</div>
+    <Link to='/'>Home</Link>
+    <Link to='/films'>Films</Link>
+    <Link to='/contactus'>Contact Us</Link>
+  </div>
+  );
+}
+function ContactUs() {
+  return (
+  <div>
+    <div>Contact Us</div>
+    <Link to='/'>Home</Link>
+    <Link to='/films'>Films</Link>
+    <Link to='/contactus'>Contact Us</Link>
+  </div>
+  );
+}
 export default App;
