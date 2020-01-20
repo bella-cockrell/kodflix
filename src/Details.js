e85fa60b5218cd247fb5f670a76b7f6365a4f627
bella-cockrell <heybellac@gmail.com>
2020-01-20 14:04:23 +0000

    Adding basic code structure to Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import './Gallery.js';
 import {
     BroswerRouter as Router,
-    Swtich,
+    Switch,
     Route,
     Link
   } from 'react-router-dom';
 
-  
+// export default function RoutingLinks() {
+//     return (
+//         <Router>
+//             <div>
+//              <Link to='/'>StarWars</Link>
+//             </div>
+//         </Router>
+
+//         <Switch> 
+//             <Route exact path='/'></Route>
+//             <StarWars />
+//         </Switch>
+//     )
+// }
+
+// function StarWars() {
+//     return (
+//         <div> 
+//             <h2> Star Wars</h2>
+//         </div>
+//     );
+// }
