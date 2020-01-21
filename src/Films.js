64b524d8e3610045d800966ac0633a1d0d657759
bella-cockrell <heybellac@gmail.com>
2020-01-21 14:00:01 +0000

    Adding adjustments to Films component

--- src/Films.js
+++ src/Films.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function Films() {
     return (
-        <Link to='/films'>
-            <div>Welcome to Films</div>
-            <Link to='/'>Home</Link>
-            <Link to='/films'>Films</Link>
-            <Link to='/contactus'>Contact Us</Link>
-        </Link>
+            <div>
+                <h1>Welcome to Films</h1>
+                <p className='pPage'> Here, you'll be able to find a list of films in our catalogue.</p>
+                <br/>
+                <Link to='/'>Home</Link>
+                <Link to='/contactus'>Contact Us</Link>
+            </div>
+
     );
 }
