47219844685427e905ce808bac010036ce5195e1
bella-cockrell <heybellac@gmail.com>
2020-03-01 21:23:59 +0000

    Adding npm-run-all dependency and script

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Today is a fine day.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
