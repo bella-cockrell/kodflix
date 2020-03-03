4129722705f303070847e5d065801721b0eec7c0
bella-cockrell <heybellac@gmail.com>
2020-03-03 11:44:50 +0000

    Adding not-found route

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World! Today is a fine day.'))
+app.get('/', (req, res) => res.send('Hello World! Today is a fine day.'));
+//req = request, res = response
+
+app.get('/rest/not-found', (req, res) => {
+  res.send('Not found');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
