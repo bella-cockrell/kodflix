9d6d55b598395dbbeff0b597728f0b92912ac68e
bella-cockrell <heybellac@gmail.com>
2020-03-03 16:17:08 +0000

    Adding exporting module for notfound

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const notfound = require('./notfound.js');
 
 app.get('/', (req, res) => res.send('Hello World! Today is a fine day.'));
 //req = request, res = response
 
-app.get('/rest/not-found', (req, res) => {
-  res.send('Not found');
+app.get('/rest/notfound', (req, res) => {
+  res.send(notfound);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
